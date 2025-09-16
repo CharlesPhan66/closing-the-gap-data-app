73e3a3eea4744e1ce3818881e972eaf0731622fa
Charles Phan <s4131861@rmit.edu.vn>
2025-09-16 10:18:42 +0700

    Charles/ Clean up main branch before merging

--- src/main/java/app/LGA.java
+++ src/main/java/app/LGA.java
@@ -1,39 +1,5 @@
 package app;
 
-/**
- * Class represeting a LGA from the Studio Project database
- * In the template, this only uses the code and name for 2016
- *
- * @author Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
- */
 public class LGA {
-   // LGA Code
-   private String code;
 
-   // LGA Name
-   private String name;
-
-   // LGA Year
-   private int year;
-
-   /**
-    * Create an LGA and set the fields
-    */
-   public LGA(String code, String name, int year) {
-      this.code = code;
-      this.name = name;
-      this.year = year;
-   }
-
-   public String getCode() {
-      return code;
-   }
-
-   public String getName() {
-      return name;
-   }
-
-   public int getYear() {
-      return year;
-   }
 }
